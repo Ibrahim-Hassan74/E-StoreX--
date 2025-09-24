1cc4708e92a9769b6dfdd7f6d4ba7beaccb84f15
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-09-24 19:42:58 +0300

    feat(router): start implementing app routing setup
    Initialized basic routing configuration and integrated RouterModule.
    Did some initial work preparing routes structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavBarComponent } from './core/nav-bar/navbar.component';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
-import { UUID } from 'crypto';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  httpClient = inject(HttpClient);
-  categories: { id: UUID; name: string }[] = [];
-
-  baseUrl = `${environment.baseURL}`;
-  getCategories() {
-    return this.httpClient.get(`${this.baseUrl}/categories`).subscribe({
-      next: (data) => {
-        console.log(this.baseUrl);
-        this.categories = data as { id: UUID; name: string }[];
-        console.log(data);
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      },
-      complete: () => {
-        console.log('Request completed');
-      },
-    });
-  }
-  ngOnInit(): void {
-    this.getCategories();
-  }
-}
+export class AppComponent {}
