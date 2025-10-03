479817ef992359cf3b6f028a4bd823a4b098cef1
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-10-03 21:03:06 +0300

    feat: implement touch-flip product cards with auto-reset and safe cleanup using DestroyRef

--- src/app/features/product/product-card/product-card.component.ts
+++ src/app/features/product/product-card/product-card.component.ts
@@ -1,8 +1,10 @@
 import {
   Component,
   CUSTOM_ELEMENTS_SCHEMA,
+  DestroyRef,
+  inject,
   input,
-  OnInit,
+  signal,
 } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { LucideAngularModule } from 'lucide-angular';
@@ -17,4 +19,35 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductCardComponent {
   product = input.required<Product>();
+  flipped = signal(false);
+  private flipTimeout?: any;
+  private destroyRef = inject(DestroyRef);
+  static currentFlipped?: ProductCardComponent;
+  onTouchStart(event: TouchEvent) {
+    if (
+      ProductCardComponent.currentFlipped &&
+      ProductCardComponent.currentFlipped !== this
+    ) {
+      ProductCardComponent.currentFlipped.resetFlip();
+    }
+    this.flipped.set(true);
+    ProductCardComponent.currentFlipped = this;
+
+    if (this.flipTimeout) clearTimeout(this.flipTimeout);
+
+    this.flipTimeout = setTimeout(() => {
+      this.resetFlip();
+    }, 3000);
+    this.destroyRef.onDestroy(() => {
+      if (this.flipTimeout) clearTimeout(this.flipTimeout);
+    });
+  }
+  resetFlip() {
+    this.flipped.set(false);
+    if (this.flipTimeout) clearTimeout(this.flipTimeout);
+    this.flipTimeout = undefined;
+    if (ProductCardComponent.currentFlipped === this) {
+      ProductCardComponent.currentFlipped = undefined;
+    }
+  }
 }
